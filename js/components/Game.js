3d1d3d253c412e2fb5403c31f978a044aa1bea02
mrdingoro <mrdingoro@gmail.com>
2023-01-01 14:32:36 +0300

    доболвение репы для проверка коллизии

---
+++ js/components/Game.js
@@ -0,0 +1,125 @@
+import Platform from "./Platform.js";
+import { PlatformMap, conditionMap } from "../utils/levels/index.js";
+
+import playerImg from "../../img/playerr.png";
+import startImage from "../../img/startImage1.jpg";
+
+const introductionImages = [
+    { imageUrl: "../../img/startImage1.jpg", input: false },
+    { imageUrl: "../../img/startImage1.jpg", input: false },
+    { imageUrl: "../../img/startImage1.jpg", input: false },
+    { imageUrl: "../../img/startImage1.jpg", input: false },
+];
+
+export default class Game {
+    constructor ( player, controller, listenerUp, listenerDown ) {
+        this.player = player;
+        this.controller = controller;
+        this.listenUp = listenerUp;
+        this.listenDown = listenerDown;
+
+        this.context = null;
+
+        this.stats = {
+            name: undefined,
+            gravity: 0.5,
+            lvl: 0,
+        }
+
+        this.startIntroduction();
+    }
+
+    startIntroduction() {
+
+        const image = document.createElement('img');
+        image.src  = startImage;
+        image.setAttribute('width', '1280px');
+        image.setAttribute('height', '720px');
+        image.setAttribute('position', 'absolute');
+        image.setAttribute('top', '20%');
+        image.setAttribute('left', '20%');
+
+        document.getElementById('body').setAttribute('margin', '0 auto');
+        document.getElementById('body').appendChild(image);
+        const listenerID = this.listenDown(this);
+    }
+
+    setup() {
+        const canvas = document.querySelector('canvas');
+        this.context = canvas.getContext('2d');
+
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+
+        //спросить имя и дать бусты андрею и диме
+        //this.stats.name = prompt('Как вас зовут?')
+        //alert(this.stats.name)
+
+        //вывести правила
+        //...
+
+        this.player = new this.player(
+            this.context,
+            this.stats.gravity,
+            this.winLevel.bind(this),
+            playerImg
+        );
+
+        this.controller = new this.controller(this.context);
+        //
+        //здесь должен быть вызов this.setupSprites()
+        this.sprites = [this.player.getSprite()];
+
+        this.listenDown(this.player);
+        this.listenUp(this.player);
+
+        this.start();
+    }
+
+    start() {
+        // это уже последний или еще нет?
+        this.startNewLevel();
+    }
+
+    startNewLevel() {
+        this.player.setLevelConditions(conditionMap[this.stats.lvl]);
+
+        const platforms = PlatformMap[this.stats.lvl].map(element => {
+            return new Platform(this.context, element)
+        });
+
+        this.controller.animate([this.player, ...platforms, ...this.sprites], this.stats.lvl);
+    }
+
+    winLevel() {
+        // поздравления
+        // ...
+
+        // плюс уровень
+        this.stats.lvl = this.stats.lvl + 1;
+        console.log('level', this.stats.lvl)
+
+        // повторить сначала
+        this.start();
+    }
+
+    winGame() {
+        // финальные поздравления
+        // конец
+    }
+
+    lose() {
+        // предложить попробовать еще раз
+
+        // если да
+        // повторить сначала
+        this.start();
+
+        // если нет
+        // утешение)
+    }
+
+    setupSprites(){
+        //вынести логику по спрайтам
+    }
+}
---
+++ js/components/Player.js
@@ -0,0 +1,114 @@
+import Sprite from "./Sprite.js";
+
+export default class Player {
+    constructor(context, gravity, winCallback, url) {
+        this.position = {
+            x: 100,
+            y: 100,
+        }
+
+        this.velocity = {
+            x: 0,
+            y: 0,
+        }
+
+        this.keys = {
+            right: {
+                pressed: false,
+            },
+            left: {
+                pressed: false,
+            },
+        }
+
+        this.width = 48;
+        this.height = 48;
+
+        this.awaited = 0;
+        this.activated = 0;
+
+        this.context = context;
+        this.gravity = gravity;
+
+        this.winCallback = winCallback;
+        this.spriteUrl = url;
+
+        this.start();
+    }
+
+    start() {
+        const img = new Image();
+        img.src = this.spriteUrl;
+
+        this.sprite = new Sprite(this.context, img, 4, 6, 48, 48, this.position.x, this.position.y);
+    }
+
+    update() {
+        this.sprite.update();
+        this.sprite.updatePosition(this.position.x, this.position.y);
+
+        this.position.y += this.velocity.y;
+        this.position.x += this.velocity.x;
+
+        if (this.position.y + this.height + this.velocity.y <=
+            this.context.canvas.height) {
+                this.velocity.y += this.gravity;
+            }
+        else {
+            this.velocity.y = 0;
+        }
+
+    }
+
+    animate() {
+        this.update();
+
+        if (this.keys.left.pressed) {
+            this.goLeft();
+        }
+        else if (this.keys.right.pressed) {
+            this.goRight();
+        }
+        else this.stop();
+    }
+
+    jump() {
+        const jumpCondition = this.velocity.y === 0 && (this.position.y + this.height + this.velocity.y >= this.context.canvas.height);
+
+        if (jumpCondition) {
+            this.velocity.y -= 15;
+        }
+
+        this.activate();
+    }
+
+    goLeft() {
+        this.velocity.x = -5;
+    }
+
+    goRight() {
+        this.velocity.x = 5;
+    }
+
+    stop() {
+        this.velocity.x = 0
+    }
+
+    activate() {
+        this.activated = this.activated + 1;
+
+        if (this.awaited === this.activated) {
+            this.winCallback();
+        }
+    }
+
+    setLevelConditions(awaited){
+        this.activated = 0;
+        this.awaited = awaited;
+    }
+
+    getSprite(){
+        return this.sprite.get();
+    }
+
+}
